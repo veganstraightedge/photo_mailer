2f9d525dfbef4b2dafd8e85e5d199aea09880531
Shane Becker <veganstraightedge@gmail.com>
2022-07-27 15:49:13 -0700

    Add stubbed home/terms/privacy pages

---
+++ app/controllers/about_controller.rb
@@ -0,0 +1,10 @@
+class AboutController < ApplicationController
+  def home
+  end
+
+  def terms
+  end
+
+  def privacy
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  # Homepage
+  root "about#home"
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  # Administrivia
+  get "terms",   to: "about#terms",   as: :terms
+  get "privacy", to: "about#privacy", as: :privacy
 end
