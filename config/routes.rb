62bffe4f293ffbc4847e65316184c410a303a2f2
Shane Becker <veganstraightedge@gmail.com>
2022-07-27 18:24:33 -0700

    Customize Devise routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,37 @@
 Rails.application.routes.draw do
-  # Homepage
-  root "about#home"
+  # Auth
+  devise_for :user,
+             path:        "/",
+             path_names:  {
+               confirmation: "confirm",
+               password:     "reset",
+               registration: "register",
+               sign_in:      "signin",
+               sign_up:      "signup",
+               sign_out:     "signout"
+             },
+             controllers: {
+               sessions: "users/sessions"
+             }
+
+  devise_scope :user do
+    get "signup",  to: "devise/registrations#new"
+    get "signin",  to: "devise/sessions#new"
+    get "signout", to: "devise/sessions#destroy"
+    get "forgot",  to: "devise/passwords#new"
+
+    get "settings/account", to: "devise/registrations#edit", as: :account_settings
+  end
+
+  # Homepage for not signed-in users
+  unauthenticated do
+    root "home#index"
+  end
+
+  # Homepage for signed-in users
+  authenticated :user do
+    root "dashboard#index", as: :authenticated_root
+  end
 
   # Administrivia
   get "terms",   to: "about#terms",   as: :terms
