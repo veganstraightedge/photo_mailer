9a4c51071b3af85be62f025b88086ca46b22eb9b
Shane Becker <veganstraightedge@gmail.com>
2022-07-27 19:01:28 -0700

    Removes div.form_with_errors wrapper around form fields with validation errors

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,9 @@ require_relative "application"
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+# Removes div.form_with_errors wrapper around form fields with validation errors
+#   because that breaks CSS styles, namely expected Bootstrap .classes nesting
+ActionView::Base.field_error_proc = proc do |html_tag, _instance|
+  html_tag.html_safe
+end
